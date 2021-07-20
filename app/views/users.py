b1b25b33e7633e1696c683e36ca1bf87b3488866
Bhushan <50306448+bhushm@users.noreply.github.com>
2021-07-19 23:45:18 -0400

    Add login view with template

--- app/views/users.py
+++ app/views/users.py
@@ -1,6 +1,9 @@
 from flask import (
     Blueprint,
+    render_template,
 )
+
+from .. import models, forms
 
 
 bp = Blueprint(
@@ -8,3 +11,23 @@ bp = Blueprint(
     __name__,
     url_prefix="/users",
 )
+
+
+@bp.route("/login/", methods=("GET",))
+def login():
+    form = forms.LoginForm()
+
+    return render_template(
+        "users/login.html",
+        form=form,
+    )
+
+
+@bp.route("/login/", methods=("POST",))
+def login_post():
+    ...
+
+
+@bp.route("/logout/")
+def logout():
+    ...
