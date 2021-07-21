4ae0036cf2461cf28971a6c1f885ebd032ac46bc
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-07-20 23:05:55 -0400

    Update database scripts

--- app/scripts.py
+++ app/scripts.py
@@ -1,12 +1,14 @@
 from . import models
 
-models.Model.metadata.create_all(bind=models.db_engine)
+
+def create_db():
+    models.Model.metadata.create_all(bind=models.db_engine)
 
 
 def create_users():
     user = models.User(email="b@b.com")
 
-    user.set_password("1234")
+    user.set_password("b")
 
     models.db_session.add(user)
     models.db_session.commit()
