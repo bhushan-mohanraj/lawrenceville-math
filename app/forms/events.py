5f62841539e0e75483992d8ca1e54f6d1bfb16ff
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-07-27 23:51:57 -0400

    Create event form

---
+++ app/forms/events.py
@@ -0,0 +1,35 @@
+from wtforms import Form, fields, validators
+
+from .. import models
+
+
+class EventForm(Form):
+    name = fields.StringField(
+        "Name",
+        [
+            validators.InputRequired(),
+            validators.Length(max=100),
+        ],
+    )
+
+    start = fields.DateTimeField(
+        "Start",
+        [
+            validators.InputRequired(),
+        ],
+    )
+
+    end = fields.DateTimeField(
+        "End",
+        [
+            validators.InputRequired(),
+        ],
+    )
+
+    category = fields.SelectField(
+        "Category",
+        [
+            validators.InputRequired(),
+        ],
+        choices=[(category, category.name) for category in models.Event.Category],
+    )
