efc7a19da8f4997dabcbe73181fde3deb96f646b
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-07-29 23:57:41 -0400

    Add index event view

--- app/views/events.py
+++ app/views/events.py
@@ -1,6 +1,6 @@
 from flask import (
     Blueprint,
-jsonify,
+    render_template,
 )
 
 from .. import models, forms
@@ -13,3 +13,13 @@ bp = Blueprint(
     __name__,
     url_prefix="/events",
 )
+
+
+@bp.route("/")
+def index():
+    events = [row[0] for row in models.db_session.execute(select(models.Event)).all()]
+
+    return render_template(
+        "events/index.html",
+        events=events,
+    )
