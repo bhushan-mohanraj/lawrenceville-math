04d0196d3704a94ed10bf823ff3eda53c6d75112
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-08-30 11:51:57 -0400

    Typo

--- app/forms/base.py
+++ app/forms/base.py
@@ -11,5 +11,5 @@ from wtforms.fields.html5 import (
     DateTimeLocalField,
 )
 
-# The default date format returned by the HTML% datetime field.
+# The default date format returned by the HTML5 datetime field.
 DATETIME_LOCAL_FORMAT = "%Y-%m-%dT%H:%M"
