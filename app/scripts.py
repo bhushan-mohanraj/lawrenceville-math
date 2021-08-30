306f3811b8c81776e3fc42473e6cbba2157b0a17
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-08-30 14:05:17 -0400

    Add script to create tests

--- app/scripts.py
+++ app/scripts.py
@@ -31,3 +31,24 @@ def create_events():
 
     models.db_session.add(event)
     models.db_session.commit()
+
+
+def create_tests():
+    test = models.Test(
+        name="Test",
+        start=datetime(2021, 1, 1),
+        end=datetime(2021, 1, 2),
+        category=models.Test.PRACTICE,
+    )
+
+    models.db_session.add(test)
+    models.db_session.commit()
+
+    problem = models.Problem(
+        test_id=test.id,
+        statement="What is 1 + 1?",
+        answer=2,
+    )
+
+    models.db_session.add(problem)
+    models.db_session.commit()
