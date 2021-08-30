fa104bc1517ab4845b519f8b5be8a446cd1d2d31
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-08-30 15:05:24 -0400

    Add note about bug

--- app/helpers.py
+++ app/helpers.py
@@ -6,5 +6,5 @@ def current_edt_datetime() -> datetime:
     The current date and time in EDT (Eastern Daylight Time, accounting for daylight savings).
     """
 
-    # TODO: Fix this function.
+    # TODO: Fix this function (currently correct only if the server is in EDT).
     return datetime.now()
