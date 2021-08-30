9ac8c915688b525bf6313b87af5863e99ebf9a61
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-08-30 14:06:37 -0400

    Add test form (bug fix)

--- app/forms/tests.py
+++ app/forms/tests.py
@@ -1,3 +1,46 @@
+from .. import models
+
+from .base import *
+
+
+class TestForm(Form):
+    name = StringField(
+        "Name",
+        [
+            validators.InputRequired(),
+            validators.Length(max=100),
+        ],
+    )
+
+    start = DateTimeLocalField(
+        "Start",
+        [
+            validators.InputRequired(),
+        ],
+        format=DATETIME_LOCAL_FORMAT,
+    )
+
+    end = DateTimeLocalField(
+        "End",
+        [
+            validators.InputRequired(),
+        ],
+        format=DATETIME_LOCAL_FORMAT,
+    )
+
+    category = SelectField(
+        "Category",
+        [
+            validators.InputRequired(),
+        ],
+        choices=[
+            (category, category.title()) for category in models.Test.CATEGORY_CHOICES
+        ],
+    )
+
+    submit = SubmitField(
+        "Submit",
+    )
 
 
 class AttemptForm(Form):
