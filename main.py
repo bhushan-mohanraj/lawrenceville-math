ea423c7844d02d6f0f5b7e8110886a506b70f0c3
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-09-01 14:08:25 -0400

    Add `main.py` to run scripts

---
+++ main.py
@@ -0,0 +1,12 @@
+from app import create_app
+
+
+app = create_app()
+
+with app.app_context():
+    from app.scripts import *
+
+    create_db()
+    create_users()
+    create_events()
+    create_tests()
