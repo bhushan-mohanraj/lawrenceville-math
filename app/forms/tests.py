15f290b7bb34e7343863bcbb32f3c8207e85d838
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-09-15 14:53:15 -0400

    Add `ProblemForm` and `AttemptForm`

--- app/forms/tests.py
+++ app/forms/tests.py
@@ -3,3 +3,5 @@ from app.forms.base import *
 
 
 TestForm = model_form(models.Test)
+ProblemForm = model_form(models.Problem, exclude_names=("test_id",))
+AttemptForm = model_form(models.Attempt, exclude_names=("test_id", "problem_id"))
