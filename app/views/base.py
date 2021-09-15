31807f797081fdb0e2f1f9c80285c4da720babb6
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-09-15 11:54:31 -0400

    Create base `CreateView` class

--- app/views/base.py
+++ app/views/base.py
@@ -1,5 +1,11 @@
 from abc import ABC
 
+from flask import render_template, request, url_for, redirect
+from flask.views import View
+
+from app import models
+from app.decorators import staff_required
+
 
 class CRUDBaseView(View, ABC):
     """
@@ -17,3 +23,36 @@ class CRUDBaseView(View, ABC):
 
     # The view name to redirect to.
     redirect_name: str
+
+
+class CreateView(CRUDBaseView):
+    methods = ["GET", "POST"]
+    decorators = [staff_required]
+
+    # Use the form template for rendering the create form.
+    template_name = "form.html"
+
+    def dispatch_request(self):
+        form = self.form_class(request.form)
+
+        if request.method == "POST" and form.validate():
+            model = self.model_class()
+
+            for field in form:
+                if field.name in self.model_class.__table__.columns.keys():
+                    setattr(
+                        model,
+                        field.name,
+                        field.data,
+                    )
+
+            models.db_session.add(model)
+            models.db_session.commit()
+
+            return redirect(url_for(self.redirect_name))
+
+        return render_template(
+            self.template_name,
+            title="Create " + self.model_class.__name__,
+            form=form,
+        )
