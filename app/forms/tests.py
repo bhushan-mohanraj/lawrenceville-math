5787001a44e8007e053a4aa24567911c599b6fe7
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-09-15 15:27:58 -0400

    Fix `AttemptForm`

--- app/forms/tests.py
+++ app/forms/tests.py
@@ -4,4 +4,4 @@ from app.forms.base import *
 
 TestForm = model_form(models.Test)
 ProblemForm = model_form(models.Problem, exclude_names=("test_id",))
-AttemptForm = model_form(models.Attempt, exclude_names=("test_id", "problem_id"))
+AttemptForm = model_form(models.Attempt, exclude_names=("problem_id", "user_id"))
