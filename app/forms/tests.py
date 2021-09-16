b2aff9530d3e757096a8105d6ef954c84e6695cd
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-09-15 20:07:43 -0400

    Format

--- app/forms/tests.py
+++ app/forms/tests.py
@@ -3,5 +3,13 @@ from app.forms.base import *
 
 
 TestForm = model_form(models.Test)
-ProblemForm = model_form(models.Problem, exclude_names=("test_id",))
-AttemptForm = model_form(models.Attempt, exclude_names=("problem_id", "user_id"))
+
+ProblemForm = model_form(
+    models.Problem,
+    exclude_names=("test_id",),
+)
+
+AttemptForm = model_form(
+    models.Attempt,
+    exclude_names=("problem_id", "user_id"),
+)
