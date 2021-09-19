d5cd1d2dbf4fa00eb22332d062d4b7e3b90ad9e8
Bhushan <50306448+bhushanmohanraj@users.noreply.github.com>
2021-09-18 23:49:36 -0400

    Add `GOOGLE_CLIENT_SECRETS` config variable

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,10 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY=os.urandom(32),
         DATABASE_URL=os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"),
+        # This variable should be set to the JSON string containing the Google client secrets.
+        # The JSON string can be downloaded from console.cloud.google.com/apis/credentials.
+        # This requires that OAuth client credentials have been created for the project.
+        GOOGLE_CLIENT_SECRETS=os.environ.get("GOOGLE_CLIENT_SECRETS", None),
     )
 
     with app.app_context():
